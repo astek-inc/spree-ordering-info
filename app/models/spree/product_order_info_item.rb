012c61cecd21eda912137be6e5f5ff11fb75adc3
Edwin Horneij <edwin@astekwallcovering.com>
2016-02-18 15:06:37 -0800

    Adding table_name to model.

--- app/models/spree/product_order_info_item.rb
+++ app/models/spree/product_order_info_item.rb
@@ -1,5 +1,6 @@
 module Spree
   class ProductOrderInfoItem < ActiveRecord::Base
+    self.table_name = 'order_info_items_spree_products'
     belongs_to :product
     belongs_to :order_info_item
   end
