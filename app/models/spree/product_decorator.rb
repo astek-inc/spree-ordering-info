32ddc2ad5fa24c70c19bfc42cbdd9e1d9649ff42
Edwin Horneij <edwin@astekwallcovering.com>
2016-07-29 16:21:17 -0700

    Add set_order_info_item method to product model that enforces uniqueness

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,10 @@
 module Spree
   Product.class_eval do
     has_and_belongs_to_many :order_info_items, :uniq => true
+
+    def set_order_info_item name
+      item = Spree::OrderInfoItem.find_by(name: name)
+      self.order_info_items << item unless item.nil? || self.order_info_items.include?(item)
+    end
   end
 end
