ccd323e98fee14bd4f0b61c3585bd2f39b6933aa
Edwin Horneij <edwin@astekwallcovering.com>
2016-07-29 16:20:04 -0700

    Add uniqueness constraint to HABTM relation between products and order_info_items

--- app/models/spree/order_info_item.rb
+++ app/models/spree/order_info_item.rb
@@ -1,6 +1,6 @@
 module Spree
   class OrderInfoItem < ActiveRecord::Base
     self.table_name = 'order_info_items'
-    has_and_belongs_to_many :products
+    has_and_belongs_to_many :products, :uniq => true
   end
 end
